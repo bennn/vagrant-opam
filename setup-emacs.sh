c6e42e7a5dcf8031818c4a53c45af7df3e01005b
Ben Greenman <types@ccs.neu.edu>
2015-01-17 12:28:12 -0500

    added step to compile ProofGeneral

--- setup-emacs.sh
+++ setup-emacs.sh
@@ -4,4 +4,6 @@ sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
 opam install -y ocp-index ocp-indent
 
+# Copy the default .emacs.d, compile ProofGeneral
 cp -rT $HOME/Coq311/courseware/emacs.d/ $HOME/.emacs.d/
+cd $HOME/.emacs.d/ProofGeneral-4.2; make compile
